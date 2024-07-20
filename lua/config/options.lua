c3746140062b4043c55b13a5872ddaa7381fee34
thatjpk <_@jpk.is>
2024-07-19 22:46:50 -0400

    Update TODOs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,16 +18,31 @@ vim.opt.cursorcolumn = true
 -- spellin
 vim.opt.spell = true
 
--- TODO:
+-- TODOs:
+
+-- disable the merged clipboard thing
+--     - This config puts everything that lands in the default vim register
+--       into the system clipboard.  It's honestly worse than when they're
+--       separate because your system clipboard gets clobbered by every little
+--       thing in vim (e.g., normal mode 'x', 'ciw', etc.)
 -- jwt commands
 -- j and k move within a line when wrapped?
 --   (old config used up/down for this and left j/k move between lines the
 --   traditional way.)
 -- enter-to-save
 -- shift-tab to cycle splits
--- try golang stuff
--- try python stuff
 -- F6 line num toggle (none, rel, abs)
 -- shop color schemes (also, use color scheme color for 80 col ruler)
 -- git gutter?  there has to be a lazyvim thing installed for this?
 -- make split borders more visible
+-- Language setups for:
+--     - json
+--     - yaml
+--     - markdown
+--     - terraform
+--     - toml
+--     - helm?
+--     - docker
+-- debuggers?  or just lean on vscode for that? lazygit?
+-- init.lua that plays nice with vscode?
+--
